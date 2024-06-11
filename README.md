446713bfff7284d1079807d7e529dcb17f81e04f
Leonardo Maciel <leonardomacielj@gmail.com>
2024-06-11 18:28:50 -0300

    create view of website and started logic of timer

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Pomodoro
-Um site para auxiliar pessoas na utilização da técnica de pomodoro para aumentar o foco 
+# Projeto Pomodoro
+- Um site para auxiliar pessoas na utilização da técnica de pomodoro para aumentar o foco 
+
+## O que é Pomodoro
+- "A técnica Pomodoro, desenvolvida em 1988 pelo italiano Francisco Cirillo, é um método de gestão de tempo que pode ser aplicado para diversas tarefas, seja nos estudos, seja no trabalho.
+
+- Conforme o livro de Cirillo “The Pomodoro Technique”, a técnica foi criada para utilizar o tempo como um valioso aliado e realizar o que queremos fazer e do jeito que queremos fazê-lo. Além disso, capacita-nos continuamente para melhorar nossos processos.
+
+- Ao usar esse método, é possível saber não só a quantidade de atividades que são feitas, como também a qualidade. Além disso, pode-se medir o que está atrapalhando a realização das tarefas."
+
+- Veja mais sobre "Técnica Pomodoro - o que é e como funciona" em: https://brasilescola.uol.com.br/dicas-de-estudo/tecnica-pomodoro-que-e-e-como-funciona.htm
+
+## Como Funciona
+- Intercalamos o tempo de estudo e descanso, onde geralmente se separa **25 minutos** de foco total no que esta fazendo, após esse tempo finalizar se inicia **5 minutos** de descanso, onde podemos sair do ambiente de estudo, ir tomar uma água, responder mensagens e assim por diante.
+
---
+++ assets/js/main.js
@@ -0,0 +1,93 @@
+let timeNow;
+const timer = document.querySelector('.timer')
+const inputs = document.querySelectorAll('.timer input')
+
+const play = document.querySelector('.play')
+const pause = document.querySelector('.pause')
+const restart = document.querySelector('.restart')
+
+const h1 = document.querySelector('.description')
+
+
+let time;
+let started = false;
+let focus = false
+
+function updateTime() {
+    let date0 = new Date(timeNow)
+    timeNow -= 1000
+    console.log(date0.toLocaleTimeString('pt-BR', {
+        timeZone: 'UTC',
+        hour12: false
+    }))
+
+    return date0.toLocaleTimeString('pt-BR', {
+        timeZone: 'UTC',
+        hour12: false
+    })
+}
+
+function getTime() {
+    const hour = inputs[0].value * 60 * 60 * 1000
+    const min = inputs[1].value * 60 * 1000
+    const sec = inputs[2].value * 1000
+    const time = hour + min + sec
+    return time
+}
+
+function resetTimer() {
+    timer.innerHTML = ''
+
+    for (let index = 0; index < 3; index++) {
+
+        inputs[index].value = index === 1 ? '05': '00'
+
+        timer.appendChild(inputs[index])
+
+        if (index < 2) {
+            timer.innerHTML += '<span>:</span>'
+
+        }
+    }
+
+    inputs[1].value = '05'
+}
+
+function playTime() {
+
+    time = setInterval(() => {
+        timer.innerHTML = updateTime()
+    }, 1000)
+
+    setTimeout(() => {
+        clearInterval(time)
+        alert("Tempo Acabou")
+        resetTimer()
+
+        play.classList.remove('display-none')
+        pause.classList.add('display-none')
+        restart.classList.add('display-none')
+
+    }, timeNow + 1100)
+}
+
+
+play.addEventListener('click', () => {
+    if (!started) {
+        timeNow = getTime()
+        started = true
+    }
+    h1.classList.remove('hidden')
+
+    play.classList.add('display-none')
+    pause.classList.remove('display-none')
+    restart.classList.remove('display-none')
+
+    playTime()
+})
+
+pause.addEventListener('click', () => {
+
+})
+
+
